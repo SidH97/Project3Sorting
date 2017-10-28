667574d93c972868a2fa65d50e5beefddcaac401
Sid <sid97@vt.edu>
2017-10-28 17:28:48 -0400

    Finished inital setup of git and added defult class.

---
+++ Project3Sorting/src/Sorter.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * This class will sort the parsed data given.
+ *
+ * @author Siddharth Hingorani (sid97)
+ * @author Matthew Evans (matce93)
+ * 
+ * @version 10.28.2017
+ *
+ */
+public class Sorter
+{
+
+}
