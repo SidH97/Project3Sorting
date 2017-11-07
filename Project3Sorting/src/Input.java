c305b28248db241dfd7d20fa97ac78b67eca4f8c
sidhingorani <sidhingorani@172.29.71.59>
2017-11-07 14:53:46 -0500

    Added mergesort class not sure if needed

--- Project3Sorting/src/Input.java
+++ Project3Sorting/src/Input.java
@@ -16,7 +16,7 @@ public class Input {
 	
 	public Input(String records, String stats) throws FileNotFoundException
 	{
-		RandomAccessFile file = new RandomAccessFile(records, "R");
+		RandomAccessFile file = new RandomAccessFile(records, "r");
 		byte[] in = new byte[BUFFBYTES];
 		byte[] out = new byte[BUFFBYTES];
 		byte[] heap = new byte[8*BUFFBYTES];
@@ -24,5 +24,52 @@ public class Input {
 		ByteBuffer outBuffer =  ByteBuffer.wrap(out);
 		ByteBuffer heapBuffer =  ByteBuffer.wrap(heap);
 	} 
+	
+	public void read()
+	{
+		try
+        {
+			RandomAccessFile file = new RandomAccessFile
+	                ("test.txt", "r");
+	        FileChannel inChannel = aFile.getChannel();
+	        ByteBuffer buffer = ByteBuffer.allocate(1024);
+	        while(inChannel.read(buffer) > 0)
+	        {
+	            buffer.flip();
+	            for (int i = 0; i < buffer.limit(); i++)
+	            {
+	                System.out.print((char) buffer.get());
+	            }
+	            buffer.clear(); // do something with the data and clear/compact it.
+	        }
+	        inChannel.close();
+	        aFile.close();
+        }
+        catch (Exception exc)
+        {
+            System.out.println(exc);
+            System.exit(1);
+        }
+	}
+	
+	public void replacementSort()
+	{
+		heap = byte[] data;
+		inputbuffer = byte[] data;
+		Looptill input.isempty();
+		{
+			heap.heapify;
+			heap[0].pushtoout;
+			input[0].pushtoheap;
+		}
+		heap.heapify;
+		Looptll heap.isempty()
+		{
+			heap[0].pushtoout;
+		}
+		
+		
+		
+	}
 
 }
