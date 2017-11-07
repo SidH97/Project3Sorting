18a00a24cbef81d26827e853e5284265f9b3eb2d
sidhingorani <sidhingorani@172.29.71.59>
2017-11-07 12:25:41 -0500

    Work in progress

--- Project3Sorting/src/Input.java
+++ Project3Sorting/src/Input.java
@@ -1,3 +1,7 @@
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+
 /**
  * 
  */
@@ -7,5 +11,18 @@
  *
  */
 public class Input {
+	
+	private static final int BUFFBYTES = 4096;
+	
+	public Input(String records, String stats) throws FileNotFoundException
+	{
+		RandomAccessFile file = new RandomAccessFile(records, "R");
+		byte[] in = new byte[BUFFBYTES];
+		byte[] out = new byte[BUFFBYTES];
+		byte[] heap = new byte[8*BUFFBYTES];
+		ByteBuffer inBuffer =  ByteBuffer.wrap(in);
+		ByteBuffer outBuffer =  ByteBuffer.wrap(out);
+		ByteBuffer heapBuffer =  ByteBuffer.wrap(heap);
+	} 
 
 }
