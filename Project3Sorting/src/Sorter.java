ee84f2a21ed8aaaa4df8d65dcc43f0adcf4aa8fe
sidhingorani <sidhingorani@172.30.4.68>
2017-11-11 09:53:06 -0500

    Fixed outputs to run file. Need to fix:
    All our code is using byte[] we need it to use bufferbyte

--- Project3Sorting/src/Sorter.java
+++ Project3Sorting/src/Sorter.java
@@ -2,10 +2,13 @@
 /**
  * 
  */
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 /**
  * This class will sort the parsed data given.
@@ -30,9 +33,12 @@ public class Sorter
     ByteBuffer outBuffer;
     ByteBuffer heapBuffer;
     RandomAccessFile file;
+    FileChannel wChannel;
 
     Sorter(String records, String stats) throws FileNotFoundException
     {
+    	File runfile = new File("run.txt");
+        wChannel = new FileOutputStream(runfile, true).getChannel();
         file = new RandomAccessFile(records, "r");
         in = new byte[BUFFBYTES];
         out = new byte[BUFFBYTES];
@@ -105,7 +111,12 @@ public class Sorter
     {
         outputIndex = 0;
         cleanInputBuffer();
-        // needs to go to a run file
+        try {
+			wChannel.write(outBuffer);
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		}
+        outBuffer.clear();
     }
 
     private void minHeapify(int index)
