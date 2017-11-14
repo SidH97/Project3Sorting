dcf86b90dbcec7ac755c16589480dc05a8b29f1d
sidhingorani <sidhingorani@172.29.4.68>
2017-11-13 20:28:24 -0500

    here

--- Project3Sorting/src/heapsort.java
+++ Project3Sorting/src/heapsort.java
@@ -6,7 +6,7 @@ public class heapsort {
 	public static void main(String[] args) {
 		Sorter sorter = null;
 		try {
-			sorter = new Sorter("test.bin", "okay");
+			sorter = new Sorter("test.bin.cpgz", "okay");
 		} catch (IOException e) {
 			System.out.println(e.toString());
 		}
