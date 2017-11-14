973eccd5f7da6214bc7e45d04c5988ad8b89a1f7
Matthew Evans <matce93@vt.edu>
2017-11-13 21:11:14 -0500

    here

--- Project3Sorting/src/SorterTest.java
+++ Project3Sorting/src/SorterTest.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 import junit.framework.TestCase;
 
 /**
@@ -9,10 +11,19 @@ import junit.framework.TestCase;
  *
  */
 public class SorterTest extends TestCase{
-	
+	Sorter sorter;
 	public void setUp()
 	{
-		
+		try {
+			sorter = new Sorter("8block.bin", "stats.txt");
+		} catch (IOException e) {
+			System.out.println("nice try");
+		}
+	}
+	
+	public void testReplacementSelection()
+	{
+		assertTrue(sorter.replacementSelection() == 1);
 	}
 
 }
