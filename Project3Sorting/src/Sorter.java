43a1f3bb411ccae2d922997710508f6ed5bbafee
Matthew Evans <matce93@vt.edu>
2017-11-13 19:58:11 -0500

    fixed most of it

--- Project3Sorting/src/Sorter.java
+++ Project3Sorting/src/Sorter.java
@@ -300,21 +300,36 @@ public class Sorter
     	}
     	else if (run == 2) {
     		//load into merge2
+    		nextInput(0, 2048, 1, 0);
+    		nextInput(2048, 2048, 2, 0);
     		merge2(run * 4096);
     	}
     	else if (run == 3) {
+    		nextInput(0, 1024, 1, 0);
+    		nextInput(1024, 1024, 2, 0);
+    		nextInput(2048, 2048, 3, 0);
     		merge3(run * 4096);
     	}
     	else if (run == 4) {
     		//load into merge4
+    		nextInput(0, 1024, 1, 0);
+    		nextInput(1024, 1024, 2, 0);
+    		nextInput(2048, 1024, 3, 0);
+    		nextInput(3072, 1024, 4, 0);    		
     		merge4(run * 4096);
     	}
     	else if (run == 5) {
     		//load into heapBuffer
+        	nextInput(0, 1024, 1, 0);
+        	nextInput(1024, 1024, 2, 0);
+        	nextInput(2048, 1024, 3, 0);
+        	nextInput(3072, 512, 4, 0);
+        	nextInput(3584, 512, 5, 0);
     		merge5(run * 4096);
     	}
     	else if (run == 6) {
     		//load into heapBuffer
+    		
     		merge6(run * 4096);
     	}
     	else if (run == 7) {
@@ -323,13 +338,23 @@ public class Sorter
     	}
     	else if (run == 8) {
     		//load into merge8
+    		nextInput(0, 512, 1, 0);
+    		nextInput(512, 512, 2, 0;);
+    		nextInput(1024, 512, 3, 0);
+    		nextInput(1536, 512, 4, 0);
+    		nextInput(2048, 512, 5, 0);
+    		nextInput(2560, 512, 6, 0);
+    		nextInput(3072, 512, 7, 0);
+    		nextInput(3584, 512, 8, 0);
     		merge8(run * 4096);
     	}
     }
     
     private void merge2(int numRec) 
     {
+    	int depth1 = 1;
     	int x1 = 0;
+    	int depth2 = 1;
     	int hold2 = 2048;
     	int x2 = 2048;
     	for(int i = 0; i < numRec; i++)
@@ -342,9 +367,11 @@ public class Sorter
         			x1++;
         			if (x1 == hold2) {
         				//needs to get the next input and put it at the beginning (0)
-        				if (nextInput(0, 2048, 1))
+        				if (nextInput(0, 2048, 1, depth1))
         				{
         					x1 = 0;
+        					
+        					depth1++;
         				}
         				else
         				{
@@ -361,9 +388,10 @@ public class Sorter
         			x2++;
         			if (x2 == 4096) {
         				//needs to get the next input and put it at the beginning (hold2)
-        				if(nextInput(hold2, 2048, 2))
+        				if(nextInput(hold2, 2048, 2, depth2))
         				{
         					x2 = hold2;
+        					depth2++;
         				}
         				else
         				{
@@ -377,9 +405,12 @@ public class Sorter
     
     private void merge3(int numRec)
     {
+    	int depth1 = 1;
     	int x1 = 0;
+    	int depth2 = 1;
     	int hold2 = 1024;
     	int x2 = 1024;
+    	int depth3 = 1;
     	int hold3 = 2048;
         int x3 = 2048;
         for(int i = 0; i < numRec; i++)
@@ -392,9 +423,10 @@ public class Sorter
         			x1++;
         			if (x1 == hold2) {
         				//needs to get the next input and put it at the beginning (0)
-        				if (nextInput(0, 1024, 1))
+        				if (nextInput(0, 1024, 1, depth1))
         				{
         					x1 = 0;
+        					depth1++;
         				}
         				else
         				{
@@ -411,9 +443,10 @@ public class Sorter
         			x2++;
         			if (x2 == hold3) {
         				//needs to get the next input and put it at the beginning (hold2)
-        				if(nextInput(hold2, 1024, 2))
+        				if(nextInput(hold2, 1024, 2, depth2))
         				{
         					x2 = hold2;
+        					depth2++;
         				}
         				else
         				{
@@ -429,9 +462,10 @@ public class Sorter
         		x3++;
         		if (x3 == 4096) {
         			//needs to get the next input and put it at the beginning (hold3)
-        			if(nextInput(hold3, 2048, 3))
+        			if(nextInput(hold3, 2048, 3, depth3))
         			{
         				x3 = hold3;
+        				depth3++;
         			}
         			else
         			{
@@ -445,11 +479,15 @@ public class Sorter
     
     private void merge4(int numRec)
     {
+    	int depth1 = 1;
     	int x1 = 0;
+    	int depth2 = 1;
     	int hold2 = 1024;
     	int x2 = 1024;
+    	int depth3 = 1;
     	int hold3 = 2048;
         int x3 = 2048;
+        int depth4 = 1;
     	int hold4 = 3072;
         int x4 = 3072;
         for(int i = 0; i < numRec; i++)
@@ -463,9 +501,10 @@ public class Sorter
         			x1++;
         			if (x1 == hold2) {
         				//needs to get the next input and put it at the beginning (0)
-        				if (nextInput(0, 1024, 1))
+        				if (nextInput(0, 1024, 1, depth1))
         				{
         					x1 = 0;
+        					depth1++;
         				}
         				else
         				{
@@ -483,9 +522,10 @@ public class Sorter
         			x2++;
         			if (x2 == hold3) {
         				//needs to get the next input and put it at the beginning (hold2)
-        				if(nextInput(hold2, 1024, 2))
+        				if(nextInput(hold2, 1024, 2, depth2))
         				{
         					x2 = hold2;
+        					depth2++;
         				}
         				else
         				{
@@ -503,9 +543,10 @@ public class Sorter
         			x3++;
         			if (x3 == hold4) {
         				//needs to get the next input and put it at the beginning (hold3)
-        				if(nextInput(hold3, 1024, 3))
+        				if(nextInput(hold3, 1024, 3, depth3))
         				{
         					x3 = hold3;
+        					depth3++;
         				}
         				else
         				{
@@ -520,9 +561,10 @@ public class Sorter
         		x4++;
         		if (x4 == 4096) {
         			//needs to get the next input and put it at the beginning (hold4)
-        			if(nextInput(hold4, 512, 4))
+        			if(nextInput(hold4, 512, 4, depth4))
         		    {
         				x4 = hold4;
+        				depth4++;
         			}
         			else
         			{
@@ -535,13 +577,18 @@ public class Sorter
     
     private void merge5(int numRec)
     {
+    	int depth1 = 1;
     	int x1 = 0;
+    	int depth2 = 1;
     	int hold2 = 1024;
     	int x2 = 1024;
+    	int depth3 = 1;
     	int hold3 = 2048;
     	int x3 = 2048;
+    	int depth4 = 1;
     	int hold4 = 3072;
     	int x4 = 3072;
+    	int depth5 = 1;
     	int hold5 = 3584;
     	int x5 = 3584;
     	for(int i = 0; i < numRec; i++)
@@ -555,9 +602,10 @@ public class Sorter
         			x1++;
         			if (x1 == hold2) {
         				//needs to get the next input and put it at the beginning (0)
-        				if (nextInput(0, 1024, 1))
+        				if (nextInput(0, 1024, 1, depth1))
         				{
         					x1 = 0;
+        					depth1++;
         				}
         				else
         				{
@@ -575,9 +623,10 @@ public class Sorter
         			x2++;
         			if (x2 == hold3) {
         				//needs to get the next input and put it at the beginning (hold2)
-        				if(nextInput(hold2, 1024, 2))
+        				if(nextInput(hold2, 1024, 2, depth2))
         				{
         					x2 = hold2;
+        					depth2++;
         				}
         				else
         				{
@@ -595,9 +644,10 @@ public class Sorter
         			x3++;
         			if (x3 == hold4) {
         				//needs to get the next input and put it at the beginning (hold3)
-        				if(nextInput(hold3, 1024, 3))
+        				if(nextInput(hold3, 1024, 3, depth3))
         				{
         					x3 = hold3;
+        					depth3++;
         				}
         				else
         				{
@@ -615,9 +665,10 @@ public class Sorter
         			x4++;
         			if (x4 == hold5) {
         				//needs to get the next input and put it at the beginning (hold4)
-        				if(nextInput(hold4, 512, 4))
+        				if(nextInput(hold4, 512, 4, depth4))
         				{
         					x4 = hold4;
+        					depth4++;
         				}
         				else
         				{
@@ -632,9 +683,10 @@ public class Sorter
     			x5++;
         		if (x5 == 4096) {
         		//needs to get the next input and put it at the beginning (hold5)
-        			if(nextInput(hold5, 512, 5))
+        			if(nextInput(hold5, 512, 5, depth5))
         			{
         				x5 = hold5;
+        				depth5++;
         			}
         			else
         			{
@@ -648,15 +700,21 @@ public class Sorter
     
     private void merge6(int numRec)
     {
+    	int depth1 = 1;
     	int x1 = 0;
+    	int depth2 = 1;
     	int hold2 = 1024;
     	int x2 = 1024;
+    	int depth3 = 1;
     	int hold3 = 2048;
     	int x3 = 2048;
+    	int depth4 = 1;
     	int hold4 = 2560;
     	int x4 = 2560;
+    	int depth5 = 1;
     	int hold5 = 3072;
     	int x5 = 3072;
+    	int depth6 = 1;
     	int hold6 = 3584;
     	int x6 = 3584;
     	for(int i = 0; i < numRec; i++)
@@ -671,9 +729,10 @@ public class Sorter
         			x1++;
         			if (x1 == hold2) {
         				//needs to get the next input and put it at the beginning (0)
-        				if (nextInput(0, 1024, 1))
+        				if (nextInput(0, 1024, 1, depth1))
         				{
         					x1 = 0;
+        					depth1++;
         				}
         				else
         				{
@@ -692,9 +751,10 @@ public class Sorter
         			x2++;
         			if (x2 == hold3) {
         				//needs to get the next input and put it at the beginning (hold2)
-        				if(nextInput(hold2, 1024, 2))
+        				if(nextInput(hold2, 1024, 2, dpeth2))
         				{
         					x2 = hold2;
+        					depth2++;
         				}
         				else
         				{
@@ -713,9 +773,10 @@ public class Sorter
         			x3++;
         			if (x3 == hold4) {
         				//needs to get the next input and put it at the beginning (hold3)
-        				if(nextInput(hold3, 512, 3))
+        				if(nextInput(hold3, 512, 3, depth3))
         				{
         					x3 = hold3;
+        					depth3++;
         				}
         				else
         				{
@@ -734,9 +795,10 @@ public class Sorter
         			x4++;
         			if (x4 == hold5) {
         				//needs to get the next input and put it at the beginning (hold4)
-        				if(nextInput(hold4, 512, 4))
+        				if(nextInput(hold4, 512, 4, depth4))
         				{
         					x4 = hold4;
+        					depth4++;
         				}
         				else
         				{
@@ -755,9 +817,10 @@ public class Sorter
         			x5++;
         			if (x5 == hold6) {
         				//needs to get the next input and put it at the beginning (hold5)
-        				if(nextInput(hold5, 512, 5))
+        				if(nextInput(hold5, 512, 5, depth5))
         				{
         					x5 = hold5;
+        					depth5++;
         				}
         				else
         				{
@@ -772,9 +835,10 @@ public class Sorter
         		x6++;
         		if (x6 == 4096) {
         				//needs to get the next input and put it at the beginning (hold6)
-        			if(nextInput(hold6, 512, 6))
+        			if(nextInput(hold6, 512, 6, depth6))
         			{
         				x6 = hold6;
+        				depth6++;
         			}
         			else
         			{
@@ -788,17 +852,24 @@ public class Sorter
     
     private void merge7(int numRec)
     {
+    	int depth1 = 1;
     	int x1 = 0;
+    	int depth2 = 1;
     	int hold2 = 512;
     	int x2 = 512;
+    	int depth3 = 1;
     	int hold3 = 1024;
     	int x3 = 1024;
+    	int depth4 = 1;
     	int hold4 = 1536;
     	int x4 = 1536;
+    	int depth5 = 1;
     	int hold5 = 2048;
     	int x5 = 2048;
+    	int depth6 = 1;
     	int hold6 = 2560;
     	int x6 = 2560;
+    	int depth7 = 1;
     	int hold7 = 3072;
     	int x7 = 3072;
     	for(int i = 0; i < numRec; i++)
@@ -813,9 +884,10 @@ public class Sorter
         			x1++;
         			if (x1 == hold2) {
         				//needs to get the next input and put it at the beginning (0)
-        				if (nextInput(0, 512, 1))
+        				if (nextInput(0, 512, 1, depth1))
         				{
         					x1 = 0;
+        					depth1++;
         				}
         				else
         				{
@@ -834,9 +906,10 @@ public class Sorter
         			x2++;
         			if (x2 == hold3) {
         				//needs to get the next input and put it at the beginning (hold2)
-        				if(nextInput(hold2, 512, 2))
+        				if(nextInput(hold2, 512, 2, depth2))
         				{
         					x2 = hold2;
+        					depth2++;
         				}
         				else
         				{
@@ -855,9 +928,10 @@ public class Sorter
         			x3++;
         			if (x3 == hold4) {
         				//needs to get the next input and put it at the beginning (hold3)
-        				if(nextInput(hold3, 512, 3))
+        				if(nextInput(hold3, 512, 3, depth3))
         				{
         					x3 = hold3;
+        					depth3++;
         				}
         				else
         				{
@@ -876,9 +950,10 @@ public class Sorter
         			x4++;
         			if (x4 == hold5) {
         				//needs to get the next input and put it at the beginning (hold4)
-        				if(nextInput(hold4, 512, 4))
+        				if(nextInput(hold4, 512, 4, depth4))
         				{
         					x4 = hold4;
+        					depth4++;
         				}
         				else
         				{
@@ -897,9 +972,10 @@ public class Sorter
         			x5++;
         			if (x5 == hold6) {
         				//needs to get the next input and put it at the beginning (hold5)
-        				if(nextInput(hold5, 512, 5))
+        				if(nextInput(hold5, 512, 5, depth5))
         				{
         					x5 = hold5;
+        					depth5++;
         				}
         				else
         				{
@@ -918,9 +994,10 @@ public class Sorter
         			x6++;
         			if (x6 == hold7) {
         				//needs to get the next input and put it at the beginning (hold6)
-        				if(nextInput(hold6, 512, 6))
+        				if(nextInput(hold6, 512, 6, depth6))
         				{
         					x6 = hold6;
+        					depth6++;
         				}
         				else
         				{
@@ -939,9 +1016,10 @@ public class Sorter
         			x7++;
         			if (x7 == 4096) {
         				//needs to get the next input and put it at the beginning (hold7)
-        				if(nextInput(hold7, 1024, 7))
+        				if(nextInput(hold7, 1024, 7, depth7))
         				{
         					x7 = hold7;
+        					depth7++;
         				}
         				else 
         				{
@@ -957,19 +1035,27 @@ public class Sorter
     
     private void merge8(int numRec)
     {
+    	int depth1 = 1;
     	int x1 = 0;
+    	int depth2 = 1;
     	int hold2 = 512;
     	int x2 = 512;
+    	int depth3 = 1;
     	int hold3 = 1024;
     	int x3 = 1024;
+    	int depth4 = 1;
     	int hold4 = 1536;
     	int x4 = 1536;
+    	int depth5 = 1;
     	int hold5 = 2048;
     	int x5 = 2048;
+    	int depth6 = 1;
     	int hold6 = 2560;
     	int x6 = 2560;
+    	int depth7 = 1;
     	int hold7 = 3072;
     	int x7 = 3072;
+    	int depth8 = 1;
     	int hold8 = 3584;
     	int x8 = 3584;
     	for(int i = 0; i < numRec; i++)
@@ -985,9 +1071,10 @@ public class Sorter
         			x1++;
         			if (x1 == hold2) {
         				//needs to get the next input and put it at the beginning (0)
-        				if (nextInput(0, 512, 1))
+        				if (nextInput(0, 512, 1, depth1))
         				{
         					x1 = 0;
+        					depth1++;
         				}
         				else
         				{
@@ -1007,8 +1094,9 @@ public class Sorter
         			x2++;
         			if (x2 == hold3) {
         				//needs to get the next input and put it at the beginning (hold2)
-        				if(nextInput(hold2, 512, 2))
+        				if(nextInput(hold2, 512, 2, depth2))
         				{
+        					depth2++;
         					x2 = hold2;
         				}
         				else
@@ -1029,9 +1117,10 @@ public class Sorter
         			x3++;
         			if (x3 == hold4) {
         				//needs to get the next input and put it at the beginning (hold3)
-        				if(nextInput(hold3, 512, 3))
+        				if(nextInput(hold3, 512, 3, depth3))
         				{
         					x3 = hold3;
+        					depth3++;
         				}
         				else
         				{
@@ -1051,9 +1140,10 @@ public class Sorter
         			x4++;
         			if (x4 == hold5) {
         				//needs to get the next input and put it at the beginning (hold4)
-        				if(nextInput(hold4, 512, 4))
+        				if(nextInput(hold4, 512, 4, depth4))
         				{
         					x4 = hold4;
+        					depth4++;
         				}
         				else
         				{
@@ -1073,9 +1163,10 @@ public class Sorter
         			x5++;
         			if (x5 == hold6) {
         				//needs to get the next input and put it at the beginning (hold5)
-        				if(nextInput(hold5, 512, 5))
+        				if(nextInput(hold5, 512, 5, depth5))
         				{
         					x5 = hold5;
+        					depth5++;
         				}
         				else
         				{
@@ -1095,9 +1186,10 @@ public class Sorter
         			x6++;
         			if (x6 == hold7) {
         				//needs to get the next input and put it at the beginning (hold6)
-        				if(nextInput(hold6, 512, 6))
+        				if(nextInput(hold6, 512, 6, depth6))
         				{
         					x6 = hold6;
+        					depth6++;
         				}
         				else
         				{
@@ -1117,9 +1209,10 @@ public class Sorter
         			x7++;
         			if (x7 == hold8) {
         				//needs to get the next input and put it at the beginning (hold7)
-        				if(nextInput(hold7, 512, 7))
+        				if(nextInput(hold7, 512, 7, depth7))
         				{
         					x7 = hold7;
+        					depth7++;
         				}
         				else 
         				{
@@ -1134,9 +1227,10 @@ public class Sorter
     			x8++;
     			if (x8 == hold8 + hold2 - 1) { //not 100% about this
     				//needs to get the next input and put it at the beginning (hold8)
-    				if(nextInput(hold8, 512, 8))
+    				if(nextInput(hold8, 512, 8, depth8))
     				{
     					x8 = hold8;
+    					depth8++;
     				}
     				else
     				{
